9ff3f61823884fca895decb49b684ada013766ba
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 20:26:23 +0600

    learn arry how to write and findout index number

--- arry.js
+++ arry.js
@@ -1,1 +1,13 @@
-var frieds = [50, 30, 14, 25];
+var friedsAge = [50, 65, 28, 78, 44, 30, 14, 25];
+var naikeyas = [ 'opu', 'mahi', 'sabnur', 'sabana'];
+var vowels = [  'a', 'e', 'i', 'o', 'u'];
+console.log(vowels.length);
+
+/***
+ * if you want to declier arry you need to use []
+ * every word or number after use comma ,
+ * if this are word or charectar then must use single quate or dubble quate 
+ * and last use same coloan ;
+ * if you want to show variable elements length then you use variable name.length
+ * if you want to show variable index number then our use variable name[0] index number 
+ */
