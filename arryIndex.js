064b61dd474073d2220bfe0acb46f1cc030897aa
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 21:16:27 +0600

    now learn indexOf and set index[1] and value of index [0]

---
+++ arryIndex.js
@@ -0,0 +1,13 @@
+// get element value by index
+var friedsAge = [ 45, 50, 95, 47, 22, 30, 88, 29];
+// console.log(friedsAge[3]);
+var position = friedsAge;
+// set element  value by index
+position[3] = 77; 
+position[6] = 06;
+
+// find endex of an element
+ var valueP = friedsAge.indexOf(29);
+console.log(valueP);
+
+// console.log(friedsAge);
